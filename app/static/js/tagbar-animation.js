8b45d636fe197e91aaabfc2f0198ac4baa9bb690
kmilewczyk96 <kmilew0802@gmail.com>
2022-08-08 22:25:20 +0200

    Media queries added.

--- app/static/js/tagbar-animation.js
+++ app/static/js/tagbar-animation.js
@@ -1,21 +1,51 @@
 'use strict';
 
+const tagBars = document.getElementsByClassName('project-tag-bar');
 const prepareTagBarAnimation = function () {
-  const tagBars = document.getElementsByClassName('project-tag-bar');
+  const remToPixel = parseFloat(getComputedStyle(document.documentElement).fontSize);
   for (let i = 0; i < tagBars.length; i++) {
     const tagBar = tagBars[i];
     const tagBarScroll = tagBar.scrollWidth;
+    const hiddenTags = tagBar.getElementsByClassName('hidden-tag');
     if (tagBarScroll > tagBar.offsetWidth) {
-      const hiddenTags = tagBar.getElementsByTagName('li');
       for (let i = 0; i < hiddenTags.length; i++) {
         // Source of the extra 10px is pagination of tag elements.
-        const myAnimation = [{transform: `translate3d(-${tagBarScroll + 10}px, 0, 0)`}];
-        const myAnimationTiming = {duration: tagBarScroll * 30, iterations: Infinity};
-        hiddenTags[i].animate(myAnimation, myAnimationTiming)
         hiddenTags[i].classList.remove('hidden');
       }
+      const myAnimation = [{transform: `translate3d(-${tagBarScroll + remToPixel}px, 0, 0)`}];
+      const myAnimationTiming = {duration: tagBarScroll * 30, iterations: Infinity};
+      tagBar.animate(myAnimation, myAnimationTiming)
+      console.log('DELAY!');
+      console.log(remToPixel);
     }
   }
 };
 
+const clearTagBarAnimation = function () {
+  for (let i = 0; i < tagBars.length; i++) {
+    const tagBar = tagBars[i];
+    const animations = tagBar.getAnimations();
+    if (animations.length) {
+      const hiddenTags = tagBar.getElementsByClassName('hidden-tag');
+      for (let i = 0; i < hiddenTags.length; i++) {
+        // Source of the extra 10px is pagination of tag elements.
+        hiddenTags[i].classList.add('hidden');
+      }
+      for (let i = 0; i < animations.length; i++) {
+        animations[i].cancel();
+      }
+    }
+  }
+  prepareTagBarAnimation();
+};
+
+let timeout;
+const delayedTBAnimationRefresh = function () {
+  window.clearTimeout(timeout);
+  timeout = window.setTimeout(clearTagBarAnimation, 250);
+};
+
+
+
 window.addEventListener('load', prepareTagBarAnimation);
+window.addEventListener('resize', delayedTBAnimationRefresh);
